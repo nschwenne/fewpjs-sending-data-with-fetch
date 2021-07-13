b45c61a5974b79206bcd5660f529378c1d16b60b
Nicholas <nschwenne@gmail.com>
2021-07-13 07:12:32 -1000

    commit tree a0ec49be1955a1bf41d892f7d62f674f3efc7de4

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "name": name,
+      "email": email
+    })
+  })
+  .then(response => response.json())
+  .then(json => console.log(json))
+  .catch((error) => {
+    alert('Unauthorized Access');
+    return error.message
+  })
+}
+
+
+  
+  
+
+
